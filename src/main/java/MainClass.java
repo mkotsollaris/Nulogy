aa9db9e1c4c10c49bd1084d5d021c1582396c343
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2016-11-24 14:47:58 -0400

    Initial commit that contains the project configuration.

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Menelaos Kotsollaris on 2016-11-24.
+ *
+ * Class Explanation: Contains the main method.
+ */
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hey:)");
+    }
+}
