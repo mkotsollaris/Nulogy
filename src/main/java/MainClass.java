29dcb473405fe74c79c9a1269589b2184845a8df
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2016-11-24 15:24:13 -0400

    Testing Commit functionality.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,7 @@
 /**
  * Created by Menelaos Kotsollaris on 2016-11-24.
  *
- * Class Explanation: Contains the main method. Test
+ * Class Explanation: Contains the main method. Test Test
  */
 public class MainClass
 {
